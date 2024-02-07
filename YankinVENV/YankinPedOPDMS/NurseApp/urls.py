4770739960d1a2412076881e04a6b6fb3fe7693c
Bo Bo Thaw <bobothaw15@gmail.com>
2024-02-08 02:09:36 +0630

    Completed Nurse App I think I am on streak

--- YankinVENV/YankinPedOPDMS/NurseApp/urls.py
+++ YankinVENV/YankinPedOPDMS/NurseApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import waitingList_view
+from .views import waitingList_view, record_view, edit_view, ready_list
 
 
 urlpatterns = [
     path('nurseQueue/', waitingList_view, name='nurseQueue'),
+    path('nurseRecord/<int:waitingID>/', record_view, name='nurseRecord'),
+    path('nurseReadyList/', ready_list, name="readyList"),
+    path('nurseEdit/<int:waitingID>/', edit_view, name="nurseEdit"),
 ]
 
--- YankinVENV/YankinPedOPDMS/NurseApp/views.py
+++ YankinVENV/YankinPedOPDMS/NurseApp/views.py
@@ -10,7 +10,7 @@ from django.conf import settings
 @login_required
 def waitingList_view(request):
     user = request.user
-    waitingListQuery = WaitingList.objects.filter(consult_date = date.today()).order_by('queue_date_time')
+    waitingListQuery = WaitingList.objects.filter(consult_date = date.today(), isReady = False).order_by('queue_date_time')
     paginator = Paginator(waitingListQuery, 10)
     waitingLists = paginator.get_page(request.GET.get('page', 1))
     return render(request, 'NurseApp/nurse-dashboard.html', {'waitingLists': waitingLists, 'user':user, 'MEDIA_URL': settings.MEDIA_URL})
@@ -18,7 +18,52 @@ def waitingList_view(request):
 @login_required
 def record_view(request, waitingID):
     user = request.user
+    waitingListQuery = WaitingList.objects.filter(consult_date = date.today(), isReady = False).order_by('queue_date_time')
+    paginator = Paginator(waitingListQuery, 10)
+    waitingLists = paginator.get_page(request.GET.get('page', 1))
     waitingList = get_object_or_404(WaitingList, pk=waitingID)
     if request.method == 'POST':
-        waitingList.temperature = request.POST.get('')
+        try:
+            waitingList.temperature = request.POST.get('temp')
+            waitingList.weight = request.POST.get('weight')
+            waitingList.systole = request.POST.get('systole')
+            waitingList.diastole = request.POST.get('diastole')
+            waitingList.isReady = True
+            waitingList.last_edit_by = user
+            waitingList.save()
+            return render(request, 'NurseApp/nurse-dashboard.html', {'waitingLists': waitingLists, 'user':user, 'MEDIA_URL': settings.MEDIA_URL, 'success_message': "The patient's vital sign is recorded successfully."})
+        except Exception as e:
+            error_message = f"Error: {e}"
+            return render(request, 'NurseApp/nurse-dashboard.html', {'waitingLists': waitingLists, 'user':user, 'MEDIA_URL': settings.MEDIA_URL, 'error_message': error_message})
+    else:
+        return render(request, 'NurseApp/nurse-record.html', {'waitingItem': waitingList, 'user':user, 'MEDIA_URL': settings.MEDIA_URL})
 
+def ready_list(request):
+    user = request.user
+    waitingListQuery = WaitingList.objects.filter(consult_date = date.today(), isReady = True).order_by('queue_date_time')
+    paginator = Paginator(waitingListQuery, 10)
+    waitingLists = paginator.get_page(request.GET.get('page', 1))
+    return render(request, 'NurseApp/nurse-ready-list.html', {'waitingLists': waitingLists, 'user':user, 'MEDIA_URL': settings.MEDIA_URL})
+
+@login_required
+def edit_view(request, waitingID):
+    user = request.user
+    waitingListQuery = WaitingList.objects.filter(consult_date = date.today(), isReady = True).order_by('queue_date_time')
+    paginator = Paginator(waitingListQuery, 10)
+    waitingLists = paginator.get_page(request.GET.get('page', 1))
+    waitingList = get_object_or_404(WaitingList, pk=waitingID)
+    if request.method == 'POST':
+        try:
+            waitingList.temperature = request.POST.get('temp')
+            waitingList.weight = request.POST.get('weight')
+            waitingList.systole = request.POST.get('systole')
+            waitingList.diastole = request.POST.get('diastole')
+            waitingList.isReady = True
+            waitingList.last_edit_by = user
+            waitingList.save()
+            return render(request, 'NurseApp/nurse-ready-list.html', {'waitingLists': waitingLists, 'user':user, 'MEDIA_URL': settings.MEDIA_URL, 'success_message': "The patient's vital sign is updated successfully."})
+        except Exception as e:
+            error_message = f"Error: {e}"
+            return render(request, 'NurseApp/nurse-ready-list.html', {'waitingLists': waitingLists, 'user':user, 'MEDIA_URL': settings.MEDIA_URL, 'error_message': error_message})
+    else:
+        return render(request, 'NurseApp/nurse-edit.html', {'waitingItem': waitingList, 'user':user, 'MEDIA_URL': settings.MEDIA_URL})
