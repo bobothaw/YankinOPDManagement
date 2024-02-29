1617ba7f56c37c9dd5d6f5ae78707376274cba53
Bo Bo Thaw <bobothaw15@gmail.com>
2024-03-01 00:03:19 +0630

    Started doctor

--- YankinVENV/YankinPedOPDMS/DoctorApp/views.py
+++ YankinVENV/YankinPedOPDMS/DoctorApp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from ReceptionApp.models import WaitingList
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+from datetime import date
+
 
 # Create your views here.
+@login_required
+def docPatQueueView(request):
+    user = request.user
+    waitingListQuery = WaitingList.objects.filter(consult_date = date.today(), isReady = True).order_by('queue_date_time')
+    paginator = Paginator(waitingListQuery, 10)
+    waitingLists = paginator.get_page(request.GET.get('page', 1))
+    context = {
+        'waitingLists': waitingLists,
+        'user':user,
+        'MEDIA_URL': settings.MEDIA_URL
+    }
+    return render(request, 'DoctorApp/doctor-patient-queue.html', context)
+
+
+@login_required
+def diagnosisView(request, waitingID):
+    user = request.user
+    waitingPatient = get_object_or_404(WaitingList, pk=waitingID)
+    context = {
+        'waitingPatient': waitingPatient,
+        'user':user,
+        'MEDIA_URL': settings.MEDIA_URL
+    }
+    return render(request, 'DoctorApp/diagnosispage.html', context)
+
+
--- YankinVENV/YankinPedOPDMS/YankinPedOPDMS/urls.py
+++ YankinVENV/YankinPedOPDMS/YankinPedOPDMS/urls.py
@@ -35,7 +35,8 @@ urlpatterns = [
     path('receptionist/', receptionist_view, name = 'receptionist_dashboard'),
     path('ReceptionApp/', include('ReceptionApp.urls')),
     path('NurseApp/', include('NurseApp.urls')),
-    path('PharmacistApp/', include('PharmacistApp.urls'))
+    path('PharmacistApp/', include('PharmacistApp.urls')),
+    path('DoctorApp/', include('DoctorApp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
