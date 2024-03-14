7b30d918bb8d8c13087e2eae6be99d074e0b67c8
Bo Bo Thaw <bobothaw15@gmail.com>
2024-03-14 16:02:49 +0630

    I really like coding

--- YankinVENV/YankinPedOPDMS/DoctorApp/urls.py
+++ YankinVENV/YankinPedOPDMS/DoctorApp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import docPatQueueView, diagnosisView, diagnosisRecord
+from .views import docPatQueueView, diagnosisView, diagnosisRecord, diagnosisEdit, diagnosisHistoryView
 
 urlpatterns = [
     path('docPatQueue/', docPatQueueView, name='docPatQueue'),
     path('diagnosis/<int:waitingID>', diagnosisView, name='diagnose'),
     path('diagnosisRecord/<int:waitingID>', diagnosisRecord, name='diagnoserecord'),
+    path('diagnosisEdit/<int:diagnosisID>', diagnosisEdit, name='diagnosisedit'), 
+    path('diagnosisHistory/', diagnosisHistoryView, name='diagHistory'),
 ]
