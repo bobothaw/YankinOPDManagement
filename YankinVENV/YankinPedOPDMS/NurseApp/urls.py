0b3336b88150dd2f1ac7a4061e97dbcb0622223d
Bo Bo Thaw <bobothaw15@gmail.com>
2024-04-20 01:51:31 +0630

    some changes

--- YankinVENV/YankinPedOPDMS/NurseApp/urls.py
+++ YankinVENV/YankinPedOPDMS/NurseApp/urls.py
@@ -3,9 +3,9 @@ from .views import waitingList_view, record_view, edit_view, ready_list
 
 
 urlpatterns = [
-    path('nurseQueue/', waitingList_view, name='nurseQueue'),
     path('nurseRecord/<int:waitingID>/', record_view, name='nurseRecord'),
     path('nurseReadyList/', ready_list, name="readyList"),
     path('nurseEdit/<int:waitingID>/', edit_view, name="nurseEdit"),
+    path('nurseWaitingList/', waitingList_view, name="nurseWaitingList"),
 ]
 
